6d1f4c4273da35e50fd43e4b006262f3ef3ed2a0
swapnil harpale <swapnil.harpale7711@gmail.com>
2023-12-24 14:12:48 +0530

    fixed zod error

--- apps/backend/src/controllers/auth/registerController.ts
+++ apps/backend/src/controllers/auth/registerController.ts
@@ -1,8 +1,17 @@
-import { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction} from "express"
 import {registerSchema} from  "common"
 const registerController = {
     register(req: Request, res:Response, next:NextFunction){
+
         // validate the request with zod or joi
+        try {
+            const result=registerSchema.parse(req.body)
+            console.log("result", result)
+        } catch (error:any) {
+            console.log(error.errors[0].message)
+        }
+        
+     
         // authorise the request
         // check if user is already in the database
         // prepare model
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,9 +1,11 @@
 import express, {Express} from "express"
 import {PORT} from './config'
 import router from "./routes"
+import bodyParser from "body-parser"
 
 
 const app:Express = express()
+app.use(bodyParser.json())
 
 app.use("/api", router)
 
