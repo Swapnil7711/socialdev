34db49bf52616bf23d997a29d8a24663b985e2f4
swapnil harpale <swapnil.harpale7711@gmail.com>
2023-12-29 13:49:40 +0530

    updated method typemfrom post to put

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -14,7 +14,7 @@ router.post("/login", loginController.login);
 router.get("/profile", auth, userController.profile);
 router.post("/post", auth, postController.createPost);
 router.get("/post", postController.getAllPosts);
-router.post("/post/:postId", auth, postController.updatePost);
+router.put("/post/:postId", auth, postController.updatePost);
 router.delete("/post/:postId", auth, postController.deletePost);
 
 export default router;
